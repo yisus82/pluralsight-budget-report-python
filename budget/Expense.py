fd65df287c9ef4c6eb2c4770c05ae04c83c60c4d
yisus82 <djalma8@gmail.com>
2021-04-21 13:17:22 +0200

    Timing Set Comprehension and Plotting Categories

--- budget/Expense.py
+++ budget/Expense.py
@@ -48,4 +48,4 @@ class Expenses:
                                x.category == 'Mortgage')}
         food_expenses = {x for x in self.list if x.category == 'Groceries' or x.category == 'Eating Out'}
         unnecessary_expenses = set(self.list) - necessary_expenses - food_expenses
-        return necessary_expenses, food_expenses, unnecessary_expenses
+        return [necessary_expenses, food_expenses, unnecessary_expenses]
